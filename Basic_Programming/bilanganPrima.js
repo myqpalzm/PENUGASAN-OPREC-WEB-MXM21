4f6873166f176dac8f10e24a7c98f96c7d31b18f
myqpalzm <55676454+myqpalzm@users.noreply.github.com>
2021-02-19 15:57:05 +0700

    HTTP Request done! + bersih-bersih

--- Basic_Programming/bilanganPrima.js
+++ Basic_Programming/bilanganPrima.js
@@ -5,13 +5,13 @@ let counter = 1;
 function primeNumber() {
   do {
     for (let i = 2; i < number; i++) {
-        if (number % i == 0) {
-            isPrime = false;
-            break;
-        } else {
-          answer = number;
-          counter += 1;
-        }
+      if (number % i == 0) {
+        isPrime = false;
+        break;
+      } else {
+        answer = number;
+        counter += 1;
+      }
     }
   } while (counter <= 20210)
   console.log(answer);
--- HTTP_Requests_and_API_Understanding/script.js
+++ HTTP_Requests_and_API_Understanding/script.js
@@ -1,13 +1,22 @@
 $(document).ready(function() {
-  $.getJSON("https://api.quotable.io/random", function(data){
-    $('#quoteText').text(data.content);
+  $.getJSON("https://api.quotable.io/random", function(data) {
+    $('#quoteText').text('"' + data.content + '"');
     $('#authorText').text('- ' + data.author);
   });
 });
 
-$("button").click(function(){
-  $.getJSON("https://api.quotable.io/random", function(data){
-    $('#quoteText').text(data.content);
+$(".quoteBtn").click(function() {
+  $.getJSON("https://api.quotable.io/random", function(data) {
+    $('#quoteText').text('"' + data.content + '"');
     $('#authorText').text('- ' + data.author);
   });
+});
+
+$(".tweetBtn").click(function() {
+  let quote = $('#quoteText').text();
+  let author = $('#authorText').text();
+  let newTab = window.open('https://twitter.com/intent/tweet?text="' + quote + '"%0A%0A' + author, '_blank')
+  //alert('"' + quote + '" ' + author);
+  //$(location).attr('href', 'https://twitter.com/intent/tweet?text="' + quote + '" ' + author);
+  newTab.focus();
 });
